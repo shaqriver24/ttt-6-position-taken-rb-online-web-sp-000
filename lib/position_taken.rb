fd181d0805e0419f8b55255b246c8024ff55cd42
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-02-09 23:40:49 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,4 @@
 # code your #position_taken? method here!
 def position_taken?(board, idx)
-  !(board[idx] == " " || board[idx] == "" || board[idx] = nil)
+  !(board[idx] == " " || board[idx] == "" || board[idx] == nil)
 end
